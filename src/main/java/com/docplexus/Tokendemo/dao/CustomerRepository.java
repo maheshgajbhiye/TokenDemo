3e29ea2ed970780b71f9f44590be63796280a95d
a528002 <a528002@MC0YHFMC.ww930.my-it-solutions.net>
2019-06-30 19:04:12 +0530

    added changes

--- src/main/java/com/docplexus/Tokendemo/dao/CustomerRepository.java
+++ src/main/java/com/docplexus/Tokendemo/dao/CustomerRepository.java
@@ -1,5 +1,7 @@
 package com.docplexus.Tokendemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +11,6 @@ import com.docplexus.Tokendemo.bean.Priority;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 	@Query("SELECT customer FROM Customer customer WHERE customer.accountNumber=(:accountNumber)")
-	Customer findByAcountNumber(@Param("accountNumber") long accountNumber);
+	List<Customer> findByAcountNumber(@Param("accountNumber") long accountNumber);
 
 }
