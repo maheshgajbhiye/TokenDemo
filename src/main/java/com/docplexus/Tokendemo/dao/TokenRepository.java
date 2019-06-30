1c97a30cf4ee5f8e5594484023ec74ee9328bb58
a528002 <a528002@MC0YHFMC.ww930.my-it-solutions.net>
2019-06-30 19:26:59 +0530

    added changes

--- src/main/java/com/docplexus/Tokendemo/dao/TokenRepository.java
+++ src/main/java/com/docplexus/Tokendemo/dao/TokenRepository.java
@@ -2,6 +2,7 @@ package com.docplexus.Tokendemo.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -19,10 +20,12 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
 	@Query("SELECT token FROM Token token join Customer customer on token.customerid = customer.id WHERE token.status = 'A' order by customer.priorityId, token.creationDate")
 	List<Token> findByStatusAndPriority();
 
-	@Query("UPDATE Token SET staus=(:status) WHERE id=(:id)")
+	@Modifying
+	@Query("UPDATE Token SET staus=?1 WHERE id=?2")
 	void updateTokenStatus(@Param("status")String status, @Param("id") long id);
-
-	@Query("UPDATE Token SET counter=(:counterid) WHERE id=(:id)")
+	
+	@Modifying
+	@Query("UPDATE Token SET counter=?1 WHERE id=?2")
 	void updateTokenCounter(@Param("counterid") int counterNumber, @Param("id") long id);
 
 	@Query("SELECT token FROM Token token WHERE token.id=(:id)")
